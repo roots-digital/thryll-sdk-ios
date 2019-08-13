42de5ab92c2417d4e53d6b78cd667a25afbcb7f0
marvolo1995 <amancoblack@gmail.com>
2019-08-13 19:48:20 +0000

    update

---
+++ ThryllSDK/ThryllSDK.h
@@ -0,0 +1,19 @@
+//
+//  ThryllSDK.h
+//  ThryllSDK
+//
+//  Created by Kwaku Agyei Tabiri on 13/08/2019.
+//  Copyright © 2019 rootsdigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThryllSDK.
+FOUNDATION_EXPORT double ThryllSDKVersionNumber;
+
+//! Project version string for ThryllSDK.
+FOUNDATION_EXPORT const unsigned char ThryllSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThryllSDK/PublicHeader.h>
+
+
---
+++ ThryllSDKExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThryllSDKExamples
+//
+//  Created by Kwaku Agyei Tabiri on 13/08/2019.
+//  Copyright © 2019 rootsdigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
